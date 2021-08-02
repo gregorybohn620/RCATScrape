9a9b84fdf97aebf07ca4baa9f566200fe9f36be1
gregorybohn620 <gregorybohn620@utexas.edu>
2021-08-02 17:41:24 -0500

    8/2/2021 leaving office

--- news_scraper/news_scraper/spiders/cnn.py
+++ news_scraper/news_scraper/spiders/cnn.py
@@ -16,7 +16,7 @@ class CnnSpider(scrapy.Spider):
         news["url"] = response.url
         news["source"] = "CNN"
         news["author"] = response.xpath('//meta[@name="author"]/text()').get()
-
+        news["date"] =  response.xpath('//meta[@name="lastmod"]/@content').get()
         news["description"] =response.xpath('//meta[@name="description"]/').get()
 
         return news
